5850c4be10e487701d6ff6358fc9d3f998c76cf5
Iroquois Pliskin <catalinceban99@gmail.com>
2026-07-31 21:41:10 +0200

    First (and I think only) commit I will ever do in this repo. Just wanted to share this because I thought it was cool and I thought many people would like to use this tool.

---
+++ README.md
@@ -0,0 +1,4 @@
+## What does this do?
+This project, or better this single .py file, essentially operates in the following steps:
+1. Lets the user choose a .pdf file which he wants to convert in LaTex
+2. Once the user has chosen the file, it lets him write the starting and ending page in which interval the pages will be converted in LaTex
---
+++ automate_latex.py
@@ -0,0 +1,127 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+import fitz as f
+from PIL import Image
+from google import genai
+from google.genai import types
+import time
+from dotenv import load_dotenv
+
+output_name = "latex_transcrition.tex"
+gemini_model = "gemini-3.5-flash-lite"
+
+load_dotenv()
+style_path = os.getenv("STYLE_PATH")
+pdf_path = os.getenv("PDF_EXAMPLE")
+tex_path = os.getenv("TEX_EXAMPLE")
+api_key = os.getenv("GEMINI_API_KEY")
+
+root = tk.Tk()
+root.withdraw()
+
+def func(file_name: str, page_number: int):
+
+    page = doc.load_page(page_number - 1)
+    graphics = page.get_pixmap(dpi=300)
+    temp_image_path = "temp.png"
+    graphics.save(temp_image_path)
+
+    # Reading the example files
+    testo_stile = open(style_path, "r", encoding="utf-8").read()
+    testo_esempio = open(tex_path, "r", encoding="utf-8").read()
+
+    # Deligning instructions as it was a Gem, specializing the response into being what we want
+    system_instruction = f"""
+    Sei un trascrittore esperto di documenti matematici in LaTeX.
+    Il tuo compito è convertire l'image di appunti scritti a mano in codice LaTeX pulito, compilabile e rigoroso.
+
+    DEVI rispettare tassativamente gli ambienti custom, i pacchetti e lo stile definiti nel seguente file .sty:
+    --- start STILE_APPUNTI.STY ---
+    {testo_stile}
+    --- end STILE_APPUNTI.STY ---
+
+    Ecco un esempio di file .tex già strutturato secondo le mie preferenze:
+    --- start ESEMPIO.TEX ---
+    {testo_esempio}
+    --- end ESEMPIO.TEX ---
+
+    REGOLE TASSATIVE:
+    1. Rispondi ESCLUSIVAMENTE con il codice LaTeX corrispondente al contenuto dell'image.
+    2. Non aggiungere introduzioni, spiegazioni, saluti né blocchi di testo extra.
+    3. Se usi delimitatori di codice markdown (es. ```latex), fornisci solo il codice al loro interno.
+    4. Non generare il preambolo, non includere documentclass né begin document o end document. 
+    Genera ESCLUSIVAMENTE il codice relativo ai capitoli/sezioni presenti nell'image, 
+    assumendo che i pacchetti e gli ambienti siano già stati caricati nel documento padre.
+    5. Ometti TASSATIVAMENTE gli esercizi quando scritti e gli esempi lasciane al massimo uno,
+       ma anche in quel caso cerca di ridurli il più possibile salvo esempi davvero importanti.
+    6. Nonostante tu debba ciecamente rispettare ogni singola parola delle immagini di input, voglio che comunque
+       tu mantenga una certa formalità, ad esempio limitando il più possibile il grassetto alle cose essenziali, 
+       non utilizzando mai il caps lock nonostante nell'image sia utilizzato (devi rendere la dispensa professionale,
+       quella scritta su carta è un conto ma su latex ha un altro impatto).
+    7. Per la matematica, salvo formule molto importanti, cerca di scriverla per quanto possibile inline. Usa la matematica
+       centrata solo quando sono formule davvero importanti ed essenziali del campo dell'analisi.
+    8. Fai forte ed estremamente riferimento alla dispensa di analisi caricata come esempio: devi seguire a pieno
+       lo stile di quanto ci fosse già scritto, in modo che la continuazione di tutto sembri naturale, scritta dalla stessa persona.
+       Dunque mantieni anche gli stili di indentazione e di uso di grassetti e corsivi.
+    """
+
+    user_prompt = "Trascrivi fedelmente ed interamente il contenuto di questa page in codice LaTeX."
+
+    # Loading the image we have to send Gemini for it to traduce it
+    image = Image.open(temp_image_path)
+
+    # Initializing Gemini: the API Key gets loaded from the system environment variables
+    client = genai.Client()
+
+    # Sending the prompt and saving the answer
+    response = client.models.generate_content(
+        model=gemini_model,
+        contents=[image, user_prompt, pdf_path],
+        config=types.GenerateContentConfig(
+            system_instruction=system_instruction,
+            temperature=0.1,
+        )
+    )
+
+    latex_code = response.text
+
+    # To remove occurrences of markdown text left from the Gemini reposnse
+    if latex_code.startswith("```"):
+        rows = latex_code.splitlines()
+        if rows[0].startswith("```"):
+            rows = rows[1:]
+        if rows and rows[-1].startswith("```"):
+            rows = rows[:-1]
+        latex_code = "\n".join(rows)
+
+    # Saving the response (that basically contains only latex code)
+    # into a file that will progressively contain all of the info
+    with open(output_name, "a", encoding="utf-8") as f_out:
+        f_out.write("\n% --- NUOVA page ---\n\n" + latex_code)
+
+    if os.path.exists(temp_image_path):
+        os.remove(temp_image_path)
+
+
+file_name = filedialog.askopenfilename(
+        title="Seleziona il PDF degli appunti",
+        filetypes=[("PDF Files", "*.pdf")]
+)
+
+doc = f.open(file_name)
+
+start = int(input("page di inizio: "))
+end = int(input("page di fine: "))
+end = min(end, len(doc))
+print(end)
+
+for i in range(start, end+1):
+
+    # Priting just to track where we at
+    print(f"page: {i - start + 1}")
+    
+    func(file_name, i)
+
+    # Waiting 5 seconds not to consume all the RPM tokens
+    time.sleep(5)
